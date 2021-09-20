c9b54d9d99d4645c6e760c919b0f3ecc94658f0e
Jesús Peña <jesuspm92@gmail.com>
2021-09-20 17:53:06 +0200

    Added a machine learning type of strategy (to be implemented later)

---
+++ strategies/ml.py
@@ -0,0 +1,25 @@
+""" This file contains Machine Learning based strategies. Binary files
+containing the models should be stored in an accessible path. """
+
+from strategies.strategy import Strategy
+
+
+class ThreeBarsNN(Strategy):
+    def __init__(self, data, events, portfolio):
+        self.data = data
+        self.symbol_list = self.data.symbol_list
+        self.events = events
+        self.portfolio = portfolio
+        self.name = 'Three Bars Neural Network'
+
+        self.bought = self._calculate_initial_bought()
+
+    def _calculate_initial_bought(self):
+        bought = {}
+        for symbol in self.symbol_list:
+            bought[symbol] = False
+
+        return bought
+
+    def calculate_signals(self, event):
+        pass

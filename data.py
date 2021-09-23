0f8f3e820ad6bfec878654e930374785bc149701
Jesús Peña <jesuspm92@gmail.com>
2021-09-23 23:45:14 +0200

    Docstring added as in original articles of Michael Halls-Moore

--- data.py
+++ data.py
@@ -12,22 +12,60 @@ from enum import Enum
 class DataSource(Enum):
     NASDAQ = "NASDAQ"
     YAHOO = "YAHOO"
+    KRAKEN = "KRAKEN"
 
 
 class DataHandler(metaclass=ABCMeta):
+    """
+    DataHandler is an abstract base class providing an interface for
+    all subsequent (inherited) data handlers (both live and historic).
+
+    The goal of a (derived) DataHandler object is to output a generated
+    set of bars (OLHCVI) for each symbol requested.
+
+    This will replicate how a live strategy would function as current
+    market data would be sent "down the pipe". Thus a historic and live
+    system will be treated identically by the rest of the backtesting suite.
+    """
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def get_latest_data(self, symbol, N=1):
+        """
+        Returns the last N bars from the latest_symbol list,
+        or fewer if less bars are available.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def update_latest_data(self):
+        """
+        Pushes the latest bar to the latest symbol structure
+        for all symbols in the symbol list.
+        """
         raise NotImplementedError
 
 
 class HistoricCSVDataHandler(DataHandler):
+    """
+    HistoricCSVDataHandler is designed to read CSV files for
+    each requested symbol from disk and provide an interface
+    to obtain the "latest" bar in a manner identical to a live
+    trading interface.
+    """
     def __init__(self, events, csv_dir, symbol_list, source=DataSource.NASDAQ):
+        """
+        Initialises the historic data handler by requesting
+        the location of the CSV files and a list of symbols.
+
+        It will be assumed that all files are of the form
+        'symbol.csv', where symbol is a string in the list.
+
+        Parameters:
+        events - The Event Queue.
+        csv_dir - Absolute directory path to the CSV files.
+        symbol_list - A list of symbol strings.
+        """
         self.events = events
         self.csv_dir = csv_dir
         self.symbol_list = symbol_list
@@ -44,6 +82,13 @@ class HistoricCSVDataHandler(DataHandler):
         self._open_convert_csv_files(source)
 
     def _open_convert_csv_files(self, source):
+        """
+        Opens the CSV files from the data directory, converting
+        them into pandas DataFrames within a symbol dictionary.
+
+        For this handler it will be assumed that the data is
+        taken from DTN IQFeed. Thus its format will be respected.
+        """
         combined_index = None
         for symbol in self.symbol_list:
             if source == DataSource.NASDAQ:
@@ -66,16 +111,28 @@ class HistoricCSVDataHandler(DataHandler):
             self.symbol_data[symbol] = self.symbol_dataframe[symbol].iterrows()
 
     def _get_new_data(self, symbol):
+        """
+        Returns the latest bar from the data feed as a tuple of
+        (sybmbol, datetime, open, low, high, close, volume).
+        """
         for row in self.symbol_data[symbol]:
             yield tuple([symbol, row[0], row[1][0]])
 
     def get_latest_data(self, symbol, N=1):
+        """
+        Returns the last N bars from the latest_symbol list,
+        or N-k if less available.
+        """
         try:
             return self.latest_symbol_data[symbol][-N:]
         except KeyError:
             print("{symbol} is not a valid symbol.").format(symbol=symbol)
 
     def update_latest_data(self):
+        """
+        Pushes the latest bar to the latest_symbol_data structure
+        for all symbols in the symbol list.
+        """
         for symbol in self.symbol_list:
             data = None
             try:
--- event.py
+++ event.py
@@ -1,14 +1,38 @@
 class Event:
+    """
+    Event is base class providing an interface for all subsequent
+    (inherited) events, that will trigger further events in the
+    trading infrastructure.
+    """
     pass
 
 
 class MarketEvent(Event):
+    """
+    Handles the event of receiving a new market update with
+    corresponding bars.
+    """
     def __init__(self):
+        """
+        Initialises the MarketEvent.
+        """
         self.type = 'MARKET'
 
 
 class SignalEvent(Event):
+    """
+    Handles the event of sending a Signal from a Strategy object.
+    This is received by a Portfolio object and acted upon.
+    """
     def __init__(self, symbol, datetime, signal_type, quantity):
+        """
+        Initialises the SignalEvent.
+
+        Parameters:
+        symbol - The ticker symbol, e.g. 'GOOG'.
+        datetime - The timestamp at which the signal was generated.
+        signal_type - 'LONG' or 'SHORT'.
+        """
         self.type = 'SIGNAL'
         self.symbol = symbol
         self.datetime = datetime
@@ -18,7 +42,24 @@ class SignalEvent(Event):
 
 # TODO: Añadir evento tipo "permuta" (cambiar una cripto por otra)
 class OrderEvent(Event):
+    """
+    Handles the event of sending an Order to an execution system.
+    The order contains a symbol (e.g. GOOG), a type (market or limit),
+    quantity and a direction.
+    """
     def __init__(self, symbol, order_type, quantity, direction):
+        """
+        Initialises the order type, setting whether it is
+        a Market order ('MKT') or Limit order ('LMT'), has
+        a quantity (integral) and its direction ('BUY' or
+        'SELL').
+
+        Parameters:
+        symbol - The instrument to trade.
+        order_type - 'MKT' or 'LMT' for Market or Limit.
+        quantity - Non-negative integer for quantity.
+        direction - 'BUY' or 'SELL' for long or short.
+        """
         self.type = 'ORDER'
         self.symbol = symbol
         self.order_type = order_type
@@ -26,6 +67,9 @@ class OrderEvent(Event):
         self.direction = direction
 
     def print_order(self):
+        """
+        Outputs the values within the Order.
+        """
         # print(
         #     "Order: Symbol={0}, Type={1}, Quantity={2}, Direction={3}"
         # ).format(self.symbol, self.order_type, self.quantity, self.direction)
@@ -34,8 +78,32 @@ class OrderEvent(Event):
 
 
 class FillEvent(Event):
+    """
+    Encapsulates the notion of a Filled Order, as returned
+    from a brokerage. Stores the quantity of an instrument
+    actually filled and at what price. In addition, stores
+    the commission of the trade from the brokerage.
+    """
     def __init__(self, timeindex, symbol, exchange, quantity, direction,
                  fill_cost, commission=None):
+        """
+        Initialises the FillEvent object. Sets the symbol, exchange,
+        quantity, direction, cost of fill and an optional
+        commission.
+
+        If commission is not provided, the Fill object will
+        calculate it based on the trade size and Interactive
+        Brokers fees.
+
+        Parameters:
+        timeindex - The bar-resolution when the order was filled.
+        symbol - The instrument which was filled.
+        exchange - The exchange where the order was filled.
+        quantity - The filled quantity.
+        direction - The direction of fill ('BUY' or 'SELL')
+        fill_cost - The holdings value in dollars.
+        commission - An optional commission sent from IB.
+        """
         self.type = 'FILL'
         self.timeindex = timeindex
         self.symbol = symbol
@@ -50,6 +118,15 @@ class FillEvent(Event):
             self.commission = commission
 
     def calculate_ib_commission(self):
+        """
+        Calculates the fees of trading based on an Interactive
+        Brokers fee structure for API, in USD.
+
+        This does not include exchange or ECN fees.
+
+        Based on "US API Directed Orders":
+        https://www.interactivebrokers.com/en/index.php?f=commission&p=stocks2
+        """
         full_cost = 1.3
         if self.quantity <= 500:
             full_cost = max(1.3, 0.013 * self.quantity)
